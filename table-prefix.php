f9476f2d38d95c75c3d3096522e94b6e40a4be5c
Adam Richards <arichard@nerdery.com>
2015-05-13 12:34:48 -0500

    error fixes

--- table-prefix.php
+++ table-prefix.php
@@ -3,7 +3,7 @@
  * CLI command to change the table prefix in the database and wp-config file
  */
 
-class Table_Prefix extends WP_CLI_Command {
+class Table_Prefix_Command extends WP_CLI_Command {
 
     /**
      * Changes the prefix of tables in the DB using default database connection
@@ -21,12 +21,12 @@ class Table_Prefix extends WP_CLI_Command {
      */
     public function change( $args, $assoc_args ) {
         global $wpdb;
-    
+
         list($new) = $args;
 
         // from the wp-config.php file ($table_prefix)
         $default_prefix = $wpdb->prefix;
-        
+
         // pull from args, if not in args use default
         $old = (!empty($assoc_args['old']) ? $assoc_args['old'] : $default_prefix);
 
@@ -34,9 +34,9 @@ class Table_Prefix extends WP_CLI_Command {
 
         // regex used to verify table prefix
         $default_regex = '/^[A-Za-z][A-Za-z0-9_]+$/';
-        
+
         $regex = (!empty($assoc_args['regex']) ? $assoc_args['regex'] : $default_prefix);
-        
+
         if (!preg_match($regex, $new)) {
             WP_CLI::error('New prefix is invalid', true);
         }
@@ -51,7 +51,7 @@ class Table_Prefix extends WP_CLI_Command {
         foreach ($tables as $table) {
             $new_tables[$table] = str_replace($old, $new, $table);
         }
-        
+
         $res = self::do_rename($new_tables);
 
         WP_CLI::success('Table prefix changed from "' . $old . '" to "' . $new . '"' . (!empty($multi) ? ' with multisite prefix "' . $multi . '"' : ''));
@@ -68,34 +68,34 @@ class Table_Prefix extends WP_CLI_Command {
     public function show( $args, $assoc_args ) {
         global $wpdb;
 
-        WP_CLI::success('The current table prefix is "' . $wpdb->prefix . '"')
+        WP_CLI::line('The current table prefix is: ' . $wpdb->prefix);
     }
 
     protected static function fetch_tables($prefix) {
         global $wpdb;
-        
-        return $wpdb->query($wpdb->prepare("SHOW TABLES FROM " . DB_NAME . " LIKE '" . $prefix . "%'"))
+
+        return $wpdb->query($wpdb->prepare("SHOW TABLES FROM " . DB_NAME . " LIKE '" . $prefix . "%'"));
     }
-    
+
     protected static function do_rename($tables) {
         global $wpdb;
-    
+
         $qry = "RENAME TABLE ";
-        
+
         $first = true;
-        
+
         foreach ( $tables as $old => $new ) {
             if (!$first) {
                 $qry .= ', ';
             }
-            
+
             $qry .= '`' . $old . '` TO `' . $new . '`';
-            
+
             $first = false;
         }
-        
+
         return $wpdb->query($qry);
     }
 }
 
-WP_CLI::add_command( 'table-prefix', 'Table_Prefix' );
+WP_CLI::add_command( 'table-prefix', 'Table_Prefix_Command' );
